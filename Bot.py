f9eb68a994aaf3a37dd1a79d2e5a7b25b24fe43a
skillneast <allworkwithfahad@gmail.com>
2026-04-30 18:27:06 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -38,7 +38,3 @@ class Bot(Client):
 if __name__ == "__main__":
     bot = Bot()
     bot.run()
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
